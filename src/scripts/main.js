b47bf1cf1fce68ef02ed7df7b884d03b20df3051
Feronika <firaferonika27@gmail.com>
2020-09-07 06:41:18 +0700

    insert, update, remove fetch

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,81 +23,67 @@ function main() {
     };
  
     const insertBook = (book) => {
-        // Membuat instance dari XMLHttpRequest
-        const xhr = new XMLHttpRequest();
- 
-        //menetapkan callback jika response sukses dan error
-        xhr.onload = function() {
-           const responseJson = JSON.parse(this.responseText);
-           showResponseMessage(responseJson.message);
-           getBook();
-         }
- 
-        xhr.onerror = function() {
-            showResponseMessage();
-        }
- 
-        // Membuat GET request dan menetapkan target URL
-        xhr.open("POST", `${baseUrl}/add`);
         
-        // Mementapkan properti Content-Type dan X-Auth-Token pada Header request
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.setRequestHeader("X-Auth-Token", "12345");
- 
-        // Mengirimkan request dan menyisipkan JSON.stringify(book) pada body
-        xhr.send(JSON.stringify(book));
+        fetch(`${baseUrl}/add`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-Auth-Token": "12345"
+            },
+            body: JSON.stringify(book)
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(responseJson => {
+            showResponseMessage(responseJson.message);
+            getBook();
+        })
+        .catch(error => {
+            showResponseMessage(error);
+        })
     };
  
     const updateBook = (book) => {
-        // Membuat instance dari XMLHttpRequest
-        const xhr = new XMLHttpRequest();
- 
-        //menetapkan callback jika response sukses dan error
-        xhr.onload = function() {
-           const responseJson = JSON.parse(this.responseText);
-           showResponseMessage(responseJson.message);
-           getBook();
-         }
- 
-        xhr.onerror = function() {
-            showResponseMessage();
-        }
- 
-        // Membuat GET request dan menetapkan target URL
-        xhr.open("PUT", `${baseUrl}/edit/${book.id}`);
         
-        // Mementapkan properti Content-Type dan X-Auth-Token pada Header request
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.setRequestHeader("X-Auth-Token", "12345");
- 
-        // Mengirimkan request dan menyisipkan JSON.stringify(book) pada body
-        xhr.send(JSON.stringify(book));
+        fetch(`${baseUrl}/edit/${book.id}`, {
+            method: "PUT",
+            headers: {
+                   "Content-Type": "application/json",
+                   "X-Auth-Token": "12345"
+            },
+            body: JSON.stringify(book)
+        })
+        .then(response => {
+               return response.json();
+        })
+        .then(responseJson => {
+               showResponseMessage(responseJson.message);
+               getBook();
+        })
+        .catch(error => {
+               showResponseMessage(error);
+        })
     };
  
     const removeBook = (bookId) => {
-        // Membuat instance dari XMLHttpRequest
-        const xhr = new XMLHttpRequest();
- 
-        //menetapkan callback jika response sukses dan error
-        xhr.onload = function() {
-           const responseJson = JSON.parse(this.responseText);
-           showResponseMessage(responseJson.message);
-           getBook();
-         }
- 
-        xhr.onerror = function() {
-            showResponseMessage();
-        }
- 
-        // Membuat DELETE request dan menetapkan target URL
-        xhr.open("DELETE", `${baseUrl}/delete/${bookId}`);
         
-        // Mementapkan properti Content-Type dan X-Auth-Token pada Header request
-        xhr.setRequestHeader("X-Auth-Token", "12345");
- 
-        // Mengirimkan request
-        xhr.send();
-    };
+        fetch(`${baseUrl}/delete/${bookId}`, {
+            method: "DELETE",
+            headers: {
+                "X-Auth-Token": "12345"
+            }
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then(responseJson => {
+                showResponseMessage(responseJson.message);
+                getBook();
+            })
+            .catch(error => {
+                showResponseMessage(error);
+            })    };
 
 
 
